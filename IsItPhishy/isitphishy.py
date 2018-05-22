5dd585c4598930637d6ef0aad7024e7997b040c1
Michael Arnold <vehk@protonmail.com>
2018-05-21 23:03:28 -0700

    Updated to make the form return on the same page.

--- IsItPhishy/isitphishy.py
+++ IsItPhishy/isitphishy.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, validators
+
+# App config.
+DEBUG = True
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'ExampleKeyHere'
 
 
-@app.route('/')
+class ReusableForm(Form):
+    name = TextField('Name:', validators=[validators.required()])
+
+
+@app.route("/", methods=['GET', 'POST'])
 def my_form():
-    return render_template('index.html')
+    form = ReusableForm(request.form)
+
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if form.validate():
+            # Save the comment here.
+            flash(f'{name} is phishy because reasons.')
+        else:
+            flash('All the form fields are required. ')
+
+    return render_template('index.html', form=form)
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = f"Pretend I checked if '{text}' was phishy or not please."
-    return processed_text
-
-
-if __name__ == '__main__':
-    app.run(debug=True)  # Debug shows your changes instantly.
+if __name__ == "__main__":
+    app.run()
