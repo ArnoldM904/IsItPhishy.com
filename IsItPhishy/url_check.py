3f6c16258038c701e074b191d191f7ca01fb8db3
Michael Arnold <vehk@protonmail.com>
2018-05-22 22:21:05 -0700

    Create url_check.py

---
+++ IsItPhishy/url_check.py
@@ -0,0 +1,20 @@
+# Python 3.6.5
+
+# from url_checker import check_url
+# Checks that entered text is a URL. If not, add "http://" and such.
+
+# from phishtank_checker import check_phishtank
+# Checks PhishTank's database to see if URL is known phishing URL.
+
+# from suspicion_checker import check_suspicion
+# Checks URL for suspicious traits common in phishing URLs.
+
+
+def examine(url):
+    # url = check_url(url)
+    # phishtank_result = check_phishtank(url)
+    # suspicion = check_suspicion(url)
+    # results = all of the above, formatted it into summarized data.
+
+    # return results
+    return url
